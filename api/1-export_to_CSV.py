1197bc0f8ba1c5d6a42aaf487b5db97ee794c8e1
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-14 22:21:36 +0000

    import CSV and add file_name defined as csv format

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,6 +2,7 @@
 
 ''' Module for retrieving API data'''
 
+import csv
 import requests
 import sys
 
@@ -10,6 +11,7 @@ if __name__ == '__main__':
     base_url = 'https://jsonplaceholder.typicode.com/'
     user_ext = '/users/{}'.format(sys.argv[1])
     todo_ext = '/todos'
+    file_name = "{}.csv".format(sys.argv[1])
 
     employee_response = requests.get(base_url+user_ext)
     employee = employee_response.json()
