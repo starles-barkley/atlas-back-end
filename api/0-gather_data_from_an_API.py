ca473c8786b8c61c40bbabe0ef2af0d7d622c09c
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-14 21:58:58 +0000

    remove whitespace

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,7 +6,7 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    
+
     base_url = 'https://jsonplaceholder.typicode.com/'
     user_ext = '/users/{}'.format(sys.argv[1])
     todo_ext = '/todos'
