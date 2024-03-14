cb87150b63a1187679900daa2215c73682230ffe
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-14 21:57:13 +0000

    fix indentation errors

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,25 +7,24 @@ import sys
 
 if __name__ == '__main__':
     
-
     base_url = 'https://jsonplaceholder.typicode.com/'
     user_ext = '/users/{}'.format(sys.argv[1])
     todo_ext = '/todos'
 
-        employee_response = requests.get(base_url+user_ext)
-        employee = employee_response.json()
-        todo_response = requests.get(base_url+user_ext+todo_ext)
-        todos = todo_response.json()
-        completed = 0
-        total = 0
-        for todo in todos:
-            total += 1
-            if todo['completed'] is True:
-                completed += 1
+    employee_response = requests.get(base_url+user_ext)
+    employee = employee_response.json()
+    todo_response = requests.get(base_url+user_ext+todo_ext)
+    todos = todo_response.json()
+    completed = 0
+    total = 0
+    for todo in todos:
+        total += 1
+        if todo['completed'] is True:
+            completed += 1
 
-        print("Employee {0} is done with tasks({1}/{2}):".format(
-            employee['name'], completed, total
-            ))
-        for todo in todos:
-            if todo['completed'] is True:
-                print("\t {}".format(todo['title']))
+    print("Employee {0} is done with tasks({1}/{2}):".format(
+        employee['name'], completed, total
+        ))
+    for todo in todos:
+        if todo['completed'] is True:
+            print("\t {}".format(todo['title']))
