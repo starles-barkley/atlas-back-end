40fe3054802153879b6aee3950df43a48b39cb74
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-14 22:45:33 +0000

    add formatting for exporting into json file

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -2,7 +2,7 @@
 
 ''' Module for retrieving API data'''
 
-import csv
+import json
 import requests
 import sys
 
@@ -11,22 +11,9 @@ if __name__ == '__main__':
     base_url = 'https://jsonplaceholder.typicode.com/'
     user_ext = '/users/{}'.format(sys.argv[1])
     todo_ext = '/todos'
-    file_name = "{}.csv".format(sys.argv[1])
+    file_name = "{}.json".format(sys.argv[1])
 
     employee_response = requests.get(base_url+user_ext)
     employee = employee_response.json()
     todo_response = requests.get(base_url+user_ext+todo_ext)
     todos = todo_response.json()
-
-    file = open(file_name, 'w')
-    csv_file = csv.writer(file, quoting=csv.QUOTE_ALL)
-    count = 0
-
-    for todo in todos:
-
-        csv_file.writerow([
-            employee['id'],
-            employee['username'],
-            todo['completed'],
-            todo['title']])
-    file.close()
