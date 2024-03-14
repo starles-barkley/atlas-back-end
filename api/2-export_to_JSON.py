0cc611d309753fd0d287ca81e495013a93d796ff
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-14 23:25:34 +0000

    fix my mistakeeee

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -11,22 +11,21 @@ if __name__ == '__main__':
     base_url = 'https://jsonplaceholder.typicode.com/'
     user_ext = '/users/{}'.format(sys.argv[1])
     todo_ext = '/todos'
-    file_name = "{}.csv".format(sys.argv[1])
+    file_name = "{}.json".format(sys.argv[1])
 
     employee_response = requests.get(base_url+user_ext)
     employee = employee_response.json()
     todo_response = requests.get(base_url+user_ext+todo_ext)
     todos = todo_response.json()
 
-    file = open(file_name, 'w')
-    csv_file = csv.writer(file, quoting=csv.QUOTE_ALL)
-    count = 0
-
+    json_key = "{}".format(employee['id'])
+    employee_json = {json_key: []}
     for todo in todos:
+        task_dict = {
+            "task": todo['title'],
+            "completed": todo['completed'],
+            "username": employee['username']}
+        employee_json[json_key].append(task_dict)
 
-        csv_file.writerow([
-            employee['id'],
-            employee['username'],
-            todo['completed'],
-            todo['title']])
-    file.close()
+        with open(file_name, 'w') as file:
+            file.write(json.dumps(employee_json))
